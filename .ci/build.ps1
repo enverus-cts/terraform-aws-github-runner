3751ea8280117930abd282f284efe89c7aa2eed4
Fernando Miguel <github@FernandoMiguel.net>
2023-07-05 15:05:16 +0100

    feat(SRE-11680): bump providers and tflint

--- .ci/build.ps1
+++ .ci/build.ps1
@@ -3,6 +3,5 @@ $OUTPUT_DIR="$TOP_DIR/lambda_output"
 
 New-Item "$OUTPUT_DIR" -ItemType Directory -ErrorAction SilentlyContinue
 
-$env:DOCKER_BUILDKIT=1 
+$env:DOCKER_BUILDKIT=1
 docker build --no-cache --target=final --output=type=local,dest="$OUTPUT_DIR" -f "$TOP_DIR/.ci/Dockerfile" "$TOP_DIR"
-
